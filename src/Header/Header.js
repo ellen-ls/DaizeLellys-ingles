f969e556a7f784f4e52376c5a3b09e8ca9949309
ellen-ls <ellen.l.s1996@gmail.com>
2023-08-23 21:54:08 -0300

    Header feita

---
+++ src/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <header className='header'>
+            <nav className='div-logo'>
+            <h2 className='logo'>Daize Lellys</h2>
+            <p className='text-logo'>Speak up to the world</p>
+            </nav>
+
+            <nav className='info-header'>
+                <a href='home'>Home</a>
+                <a href='quem sou eu'>Quem sou eu</a>
+                <a href='valores'>Valores</a>
+            </nav>
+            
+        </header>
+
+        <main>
+            <div>
+                
+            </div>
+        </main>
+      
+    </div>
+  )
+}
+
+export default Header
