7491a3d4cb0988d27b94a5e06ca71c57a59ba441
ellen-ls <ellen.l.s1996@gmail.com>
2023-09-12 22:00:27 -0300

    alteraçoes nas cores e na metodologia

--- src/components/Metodologia/Metodologia.js
+++ src/components/Metodologia/Metodologia.js
@@ -1,73 +1,21 @@
 import React from 'react'
 import './Metodologia.css'
 import { Col, Container, Row } from 'react-bootstrap';
-import inglesBiblia from './Img/livro-biblia(copia).png';
-import logo from './Img/LOGO INGLÊS ATRAVES DA BÍBLIA COLORIDO1.png';
-import daize from './Img/daize-ingles1.png'
+import { useNavigate } from 'react-router-dom';
+
 
 
 
 const Metodologia = () => {
+
+  const navigate = useNavigate()
+
   return (
     
     <div className='metodologia'>
-    <img className='logo-biblia' src={logo} alt=''></img>
-               
-            <h1 >Por que aprender Inglês através da Bíblia?</h1>
-     <Container className='text-biblia'>
+   
 
-      <Row>
-        <Col><img className='img-biblia' src={inglesBiblia} alt=''/></Col>
-        <Col> <p className='text1'>A Bíblia é um recurso linguístico excepcional para o estudo da língua inglesa. 
-    Reconhecida internacionalmente, esse material autêntico é rico em histórias, poemas, 
-    música e verdades sobre diversos temas. Sua variedade linguística proporciona uma experiência enriquecedora 
-    para os estudantes.
-    <br></br> 
-Você terá acesso a recursos audiovisuais gratuitos que ajudarão a melhorar sua compreensão auditiva e pronúncia, 
-fortalecendo sua habilidade de se comunicar, especialmente na conversação. 
-<br></br>
-Além disso, ao explorar a Bíblia em inglês, você aprenderá expressões idiomáticas e figuras de linguagem em um 
-contexto culturalmente relevante, e também desenvolverá suas habilidades de leitura e análise textual. </p>
-      
-    </Col>  
- </Row>    
- 
-</Container>
-
-      
-             
-<h1>Apresentação e Objetivo do Curso</h1>
-<Container className='text-biblia'>
-      <Row>
-        
-        <Col> 
-        <p className='text1'>Você já pensou em estudar inglês ouvindo, lendo a Bíblia e ainda cantando seus versículos?
-      
-      <br></br>
-      Este projeto traz uma abordagem cristã de ensino da Língua Inglesa que utiliza áudios, textos da Bíblia, informações bíblicas e apropria Bíblia em inglês, como veículo para o ensino.
-      
-      <br></br>
-      Uma ótima oportunidade de aprender inglês e ao mesmo tempo conhecer melhor este LIVRO tão rico.
-      </p>
-      
-        <p className='text1'>Nosso propósito é ensinar inglês através do livro mais lido, mais
-        pesquisado e publicado em toda história: <b>a Bíblia.</b> 
-        <br></br>
-        Enquanto a maioria dos livros didáticos usa diálogos planejados para trabalhar gramática e oralidade,
-        nosso curso parte da riqueza incomparável do vocabulário bíblico, suas histórias, traduções e inegável
-        influência em nosso cotidiano.
-         </p>
-
-       
-    </Col>  
-    
-    
- </Row>    
- <Col><img className='img-biblia2' src={daize} alt=''/></Col>
-  
-</Container>
-
-<h1>Método de Ensino</h1>
+    <h1>Metodologia</h1>
 <Container className='text-biblia'>
       <Row>
       
@@ -85,8 +33,12 @@ contexto culturalmente relevante, e também desenvolverá suas habilidades de le
     
  </Row>    
  
-  
+ 
 </Container>
+<div className='div-button'>
+  <button className='button-ingles'  onClick={()=>(navigate('/cursos'))}>Clique aqui e saiba mais</button>
+</div>
+
 
 </div>
     
--- src/components/Precos/Precos.js
+++ src/components/Precos/Precos.js
@@ -13,16 +13,16 @@ function Precos() {
             <Container className='container-valores' style={{ width: '100%'}}>
                 <Row>
                     <Col>
-                        <Card style={{width:'14rem',height: '40rem', textAlign: 'center',border:'none', backgroundColor:'#ccd1e0'}} className='card-valores'>
+                        <Card style={{width:'14rem',height: '40rem', textAlign: 'center',border:'none', backgroundColor:'#3c3b6e'}} className='card-valores'>
                             <Card.Img variant="top" src={vip} style={{ height: '15rem', borderRadius:'10px'}} />
                             <Card.Body className='body-precos'>
-                                <Card.Title style={{ fontSize: '18px' }} className='linha'>Individual </Card.Title>
-                                <Card.Text style={{ fontSize: '15px', textAlign:'left' }}>
+                                <Card.Title style={{ fontSize: '18px', color:'#ffffff' }} className='linha'>VIP</Card.Title>
+                                <Card.Text style={{ fontSize: '15px', textAlign:'left', color:'#ffffff' }}>
                                     Aulas síncronas, formato online, individual, suporte com o professor, plano de estudo semanal feedback semestral e certificado.
                                     Aulas de 60 minutos de duração.
 
                                 </Card.Text>
-                                <Card.Text className='text-valores'>
+                                <Card.Text className='text-valores' style={{color:'#ffffff'}}>
                                     Semestre por 6x 397.00.
                                 </Card.Text>
                                 <Card.Link href='https://api.whatsapp.com/send?phone=5583999220306&text=Olá,+Quero+contratar+o+plano+individual%21' target='blank' className='link-valores'>Contrate-me</Card.Link>
@@ -31,15 +31,15 @@ function Precos() {
                         </Card>
                     </Col>
                     <Col className='col-valores'>
-                        <Card style={{width:'14rem', height: '40rem', textAlign: 'center', border:'none', backgroundColor:'#ccd1e0' }} className='card-valores'>
+                        <Card style={{width:'14rem', height: '40rem', textAlign: 'center', border:'none', backgroundColor:'#3c3b6e' }} className='card-valores'>
                             <Card.Img variant="top" src={grupo} style={{ height: '15rem', borderRadius:'10px'}} />
                             <Card.Body>
-                                <Card.Title style={{ fontSize: '18px' }} className='linha'>Grupos até 3 pessoas</Card.Title>
-                                <Card.Text style={{ fontSize: '15px', textAlign:'left' }}>
+                                <Card.Title style={{ fontSize: '18px', color:'#ffffff' }} className='linha'>Grupos até 3 pessoas</Card.Title>
+                                <Card.Text style={{ fontSize: '15px', textAlign:'left', color:'#ffffff' }}>
                                     Aulas síncronas, formato online,  até 3 pessoas, suporte com o professor, plano de estudo semanal feedback semestral e certificado.
                                     Aulas de 60 minutos de duração.
                                 </Card.Text>
-                                <Card.Text className='text-valores'>
+                                <Card.Text className='text-valores' style={{color:'#ffffff'}}>
                                     Semestre por 6x 297.00 por pessoa.
                                 </Card.Text>
                                 <Card.Link href='https://api.whatsapp.com/send?phone=5583999220306&text=Olá,+Quero+contratar+o+plano+de+até+3+pessoas%21' target='blank' className='link-valores'>Contrate-me</Card.Link>
@@ -49,17 +49,17 @@ function Precos() {
                     </Col>
                     <Col className='col-valores'>
 
-                        <Card style={{width:'14rem',height:'40rem', textAlign: 'center', border:'none', backgroundColor:'#ccd1e0' }} className='card-valores'>
+                        <Card style={{width:'14rem',height:'40rem', textAlign: 'center', border:'none', backgroundColor:'#3c3b6e' }} className='card-valores'>
 
                             <Card.Img variant="top" src={grupos} style={{ height: '15rem', borderRadius:'10px'}} />
                             <Card.Body>
-                                <Card.Title style={{ fontSize: '18px' }} className='linha'>Grupos</Card.Title>
-                                <Card.Text style={{ fontSize: '15px', textAlign:'left' }}>
+                                <Card.Title style={{ fontSize: '18px',color:'#ffffff' }} className='linha'>Grupos</Card.Title>
+                                <Card.Text style={{ fontSize: '15px', textAlign:'left',color:'#ffffff' }}>
                                     Aulas síncronas, formato online, grupos de 5 a 10 pessoas, suporte com o professor, plano de estudo semanal feedback semestral e certificado.
                                     Aulas de 60 minutos de duração.
 
                                 </Card.Text>
-                               <Card.Text className='text-valores'>
+                               <Card.Text className='text-valores' style={{color:'#ffffff'}}>
                                    Semestre por  6x 197.00 por pessoa.
                                 </Card.Text>
                                 <Card.Link href='https://api.whatsapp.com/send?phone=5583999220306&text=Olá,+Quero+contratar+o+plano+de+5+ou+mais+pessoas%21' target='blank' className='link-valores' >Contrate-me</Card.Link>
@@ -68,16 +68,16 @@ function Precos() {
                         </Card>
                     </Col>
                     <Col className='col-valores'>
-                        <Card style={{width:'14rem', height: '45rem', textAlign: 'center', border:'none', backgroundColor:'#ccd1e0' }} className='card-valores'>
+                        <Card style={{width:'14rem', height: '45rem', textAlign: 'center', border:'none', backgroundColor:'#3c3b6e' }} className='card-valores'>
                             <Card.Img variant="top" src={consultoria} style={{ height: '15rem', borderRadius:'10px'}} />
                             <Card.Body>
-                                <Card.Title style={{ fontSize: '18px' }} className='linha'>Consultoria avaliativa</Card.Title>
-                                <Card.Text style={{ fontSize: '15px', textAlign:'left' }}>
+                                <Card.Title style={{ fontSize: '18px', color:'#ffffff' }} className='linha'>Consultoria avaliativa</Card.Title>
+                                <Card.Text style={{ fontSize: '15px', textAlign:'left', color:'#ffffff' }}>
                                     Pense numa conversa só pra você. A gente chama de Consultoria Avaliativa. Ela ajuda a entender o que você precisa e montar um plano de estudo perfeito.
                                     Com isso, os resultados que você quer,estarão ao seu alcance como nunca antes.Deixe-nos ajudá-lo a trilhar o caminho para o sucesso que você busca.
 
                                 </Card.Text>
-                                <Card.Text className='text-valores'>
+                                <Card.Text className='text-valores' style={{color:'#ffffff'}}>
                                     A partir de 200,00.
                                 </Card.Text>
                                 <Card.Link href='https://api.whatsapp.com/send?phone=5583999220306&text=Olá,+Gostaria+de+uma+consultoria+avaliativa%21' target='blank' className='link-valores'>Contrate-me</Card.Link>
