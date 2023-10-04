98e642e765edf14c5511735ff6e37f5b44a720c5
ellen-ls <ellen.l.s1996@gmail.com>
2023-10-04 12:34:55 -0300

    ajustes

--- src/components/Carrossel/Carrossel.js
+++ src/components/Carrossel/Carrossel.js
@@ -10,7 +10,7 @@ const Carrossel = () => {
         <Col className='coluna-esquerda-home'>
           <h2 className='text1-carrossel'>Aprenda Inglês</h2>
           <h2 className='text2-carrossel'>Do modo mais divertido, </h2>
-          <h2 className='text3-carrossel'>Pense fora da caixa.</h2>
+          <div className='div-text3'><h2 className='text3-carrossel'>Pense fora da</h2> <h2 className='text4-carrossel'>caixa.</h2></div>
           <button className='btn-home'><a href='https://api.whatsapp.com/send?phone=5583999220306&text=Quero+agendar+uma+aula+experimental%21' target='blank'>Comece agora!</a></button>
         </Col>
         <Col>
