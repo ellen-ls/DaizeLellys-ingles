146fa36ef1dbf1a981dd9e4acae2e9319d1ec736
ellen-ls <ellen.l.s1996@gmail.com>
2024-04-29 22:33:35 -0300

    modificado a parte de metodologia e cursos

--- src/components/Cursos/Cursos.js
+++ src/components/Cursos/Cursos.js
@@ -1,104 +1,90 @@
-import React from 'react'
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import { Card} from 'react-bootstrap'
-import biblia from './Img/ingles-atraves-da-biblia.jpeg'
-import viagens from './Img/viagens.png'
-import negocios from './Img/negocios.png'
-import musica from './Img/musica.jpeg'
-import conversa from './Img/conversa.png'
-import estrangeiro from './Img/estrangeiro.png'
-import './Cursos.css'
+import React from "react";
+
+import "react-multi-carousel/lib/styles.css";
+
+import biblia from "./Img/ingles-atraves-da-biblia.jpeg";
+import viagens from "./Img/viagens.png";
+import negocios from "./Img/negocios.png";
+import musica from "./Img/musica.jpeg";
+import conversa from "./Img/conversa.png";
+import estrangeiro from "./Img/estrangeiro.png";
+import "./Cursos.css";
+
 
 const Cursos = () => {
 
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 1024 },
-            items: 4
-        },
-        desktop: {
-            breakpoint: { max: 1024, min: 800 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 800, min: 464 },
-            items: 1
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
+  const cursos = [
+    {
+      id: 1,
+      image: biblia,
+      title: "Inglês através da Bíblia",
+      text: 'O curso de "Inglês através da Bíblia" tem o objetivo de preparar o aluno para comunicar-se de forma eficaz em inglês do Nível Iniciante A1 ao Avançado C1.O projeto traz uma abordagem cristã de ensino da Língua Inglesa que utiliza textos da Bíblia, informações bíblicas e a própria Bíblia em inglês, como veículo para o ensino.',
+    },
+    {
+      id: 2,
+      image: viagens,
+      title: "Inglês para Viagens",
+      text: 'Prepare-se para suas viagens com nosso curso de inglês especializado (Travel Course). No primeiro módulo, abordamos situações essenciais, como aeroporto, avião e alfândega, para tornar o início de sua jornada tranquilo. No segundo módulo, focamos em aspectos práticos, desde reservas de hotel até refeições em restaurantes locais e exploração da cidade. Com nossos instrutores experientes, você desenvolverá as habilidades linguísticas necessárias para uma experiência de viagem mais suave. Junte-se a nós e ganhe confiança para explorar o mundo!"',
+    },
+    {
+      id: 3,
+      image: negocios,
+      title: "Inglês para Negócios",
+      text: "É uma modalidade específica e se concentra no aprendizado do vocabulário para o meio empresarial. Ou seja, é mais formal e é usado como uma ferramenta de linguagem para negócios, mais técnica e adequada ao contexto profissional. Além disso, o estudo do Business English parte de um conhecimento prévio do idioma, pelo menos a partir do intermediário.",
+    },
+    {
+      id: 4,
+      image: musica,
+      title: "Inglês com Música",
+      text: "Você já parou para pensar em como a música é poderosa? Ouvir música é algo que a maioria de nós gosta muito e faz com enorme frequência. Só o fato de ouvirmúsicas em inglês já te ajudará a ter mais contato com o idioma e você terá a oportunidade de aumentar (e muito!) o seu vocabulário. Mesmo sendo  iniciante, você aprenderá muito vocabulário e pronúncia. ",
+    },
+    {
+      id: 5,
+      image: conversa,
+      title: "Conversation Class",
+      text: " Nossas aulas de conversação  diferem de uma aula convencional porque o foco, dessa vez, está em falar inglês. Nosso curso prepara você para conversar sobre qualquer assunto. Assim, você estará pronto para uma entrevista de emprego, uma reunião com parceiros estrangeiros ou mesmo um bate-papo com um novo amigo que não é brasileiro. À medida que você pratica inglês conversação, vai se sentindo mais seguro para essas situações.",
+    },
+    {
+      id: 6,
+      image: estrangeiro,
+      title: "Português para Estrangeiros",
+      text: "Nosso curso objetiva proporcionar a estrangeirosa oportunidade de aprender Português, como se fala no Brasil, de maneira simples e clara. Usamos situações do dia-a-dia, bem como músicas e séries, deixando o aprendizado dinâmico e funcional.",
+    },
+  ];
 
-      
-    const cursos = [{
-        id: 1,
-        image: biblia,
-        title: 'Inglês através da Bíblia',
-        text: 'O curso de "Inglês através da Bíblia" tem o objetivo de preparar o aluno para comunicar-se de forma eficaz em inglês do Nível Iniciante A1 ao Avançado C1.O projeto traz uma abordagem cristã de ensino da Língua Inglesa que utiliza textos da Bíblia, informações bíblicas e a própria Bíblia em inglês, como veículo para o ensino.'
-    },
-    {
-        id: 2,
-        image: viagens,
-        title: 'Inglês para Viagens',
-        text: 'Prepare-se para suas viagens com nosso curso de inglês especializado (Travel Course). No primeiro módulo, abordamos situações essenciais, como aeroporto, avião e alfândega, para tornar o início de sua jornada tranquilo. No segundo módulo, focamos em aspectos práticos, desde reservas de hotel até refeições em restaurantes locais e exploração da cidade. Com nossos instrutores experientes, você desenvolverá as habilidades linguísticas necessárias para uma experiência de viagem mais suave. Junte-se a nós e ganhe confiança para explorar o mundo!"'
-    },
-    {
-        id: 3,
-        image: negocios,
-        title: 'Inglês para Negócios',
-        text: 'É uma modalidade específica e se concentra no aprendizado do vocabulário para o meio empresarial. Ou seja, é mais formal e é usado como uma ferramenta de linguagem para negócios, mais técnica e adequada ao contexto profissional. Além disso, o estudo do Business English parte de um conhecimento prévio do idioma, pelo menos a partir do intermediário.'
-    },
-    {
-        id: 4,
-        image: musica,
-        title: 'Inglês com Música',
-        text: 'Você já parou para pensar em como a música é poderosa? Ouvir música é algo que a maioria de nós gosta muito e faz com enorme frequência. Só o fato de ouvirmúsicas em inglês já te ajudará a ter mais contato com o idioma e você terá a oportunidade de aumentar (e muito!) o seu vocabulário. Mesmo sendo  iniciante, você aprenderá muito vocabulário e pronúncia. '
-    },
-    {
-        id: 5,
-        image: conversa,
-        title: 'Conversation Class',
-        text: ' Nossas aulas de conversação  diferem de uma aula convencional porque o foco, dessa vez, está em falar inglês. Nosso curso prepara você para conversar sobre qualquer assunto. Assim, você estará pronto para uma entrevista de emprego, uma reunião com parceiros estrangeiros ou mesmo um bate-papo com um novo amigo que não é brasileiro. À medida que você pratica inglês conversação, vai se sentindo mais seguro para essas situações.'
-    },
-    {
-        id: 6,
-        image: estrangeiro,
-        title: 'Português para Estrangeiros',
-        text: 'Nosso curso objetiva proporcionar a estrangeirosa oportunidade de aprender Português, como se fala no Brasil, de maneira simples e clara. Usamos situações do dia-a-dia, bem como músicas e séries, deixando o aprendizado dinâmico e funcional.'
-    },
-    ]
-
-    return (
-        <div className='cursos' id='curso'>
-            <h1 className='cursos-h1'>Por que</h1><h1>meus cursos são um diferencial?</h1>
-              <Carousel responsive={responsive} className='carrossel-cursos'>          
-                    {cursos.map((curso) => (
-                         <Card key={curso.id} className='card-cursos' style={{height:'47rem'}}>
-                                <Card.Img variant="top" src={curso.image} style={{height:'18rem'}}/>
-                            <Card.Body>
-                                <Card.Title style={{textAlign:'center', borderBottom:'1px solid #d9d9d9'}}>{curso.title}</Card.Title>
-                                       
-                                        <Card.Text
-                                            style={{
-                                                fontSize: '14px',
-                                                textAlign: 'left',
-                                                paddingTop:'20px'
-                                            }}>
-                                          {curso.text}
-                                         </Card.Text>
-                            </Card.Body>
-                           
-                            </Card>  
-                           
-                    ))}
-                    </Carousel> 
-
-                    <button className='btn-cursos'><a href='https://api.whatsapp.com/send?phone=5583999220306&text=Quero+agendar+uma+aula+experimental%21' target='blank'>Adquira já!</a></button>
-              
-           </div>
-    )
-}
-export default Cursos
+  return (
+    <div className="cursos" id="curso">
+        <div className="title">
+      <h2 className="cursos-h1">Por que meus cursos são um diferencial?</h2>
+       <p>Veja minhas modalidades</p>
+      <button className="btn-cursos">
+        <a
+          href="https://api.whatsapp.com/send?phone=5583999220306&text=Quero+agendar+uma+aula+experimental%21"
+          target="blank"
+        >
+          Adquira já!
+        </a>
+      </button>
+      </div>
+     
+      <div className="container" >
+        
+        {cursos.map((curso) => (
+          <div key={curso.id} className="card-curso">
+            
+            <img className="imagem" variant="top" src={curso.image} />
+            <div className="middle">
+            <div className="text-curso">
+            <h4>{curso.title}</h4>
+            <p>{curso.text}</p>
+          </div>  
+        </div>
+        </div>
+        ))}
+      </div>
+    </div>
+   
+   
+  );
+};
+export default Cursos;
--- src/components/Metodologia/Metodologia.js
+++ src/components/Metodologia/Metodologia.js
@@ -8,15 +8,14 @@ const Metodologia = () => {
 
     <div className='metodologia'>
 
-
       <h1>Como minhas aulas funcionam</h1>
       <p>Aprenda fora da caixa de modo simples e divertido</p>
       <Container className='text-biblia'>
         <Row>
           <Col className='video-youtube-metodologia'>
             <iframe
-              height="315"
-              width="360"
+              height="400"
+              width="400"
               src="https://www.youtube.com/embed/Dwpj0o5RU1w?si=bJKiyY_KqukPRMKR"
               title="YouTube video player"
               frameborder="0"
@@ -26,24 +25,16 @@ const Metodologia = () => {
           </Col>
           <Col> <p className='text1'>Considerando que o aluno já conhece a Bíblia em seu próprio idioma, aplicaremos o princípio pedagógico em que ele parte do que lhe é 'conhecido e familiar' como ponte para a construção de um novo conhecimento.
             Mesmo sendo iniciante, ou tendo estudado inglês, mas não falando, se sentirá mais confiante e tranquilo para discorrer sobre o que já conhece naturalmente.
-
             <br></br>
             Em nossa metodologia seguimos um programa que mescla e desenvolve as quatro principais habilidade para o aprendizado
             de uma língua estrangeira (ouvir, falar, ler e escrever), enfatizando, porém, a conversação desde o primeiro dia de aula.
             <br></br>
             Utilizamos também o recurso da música para ensinar e ajudar aos alunos a memorizar versículos da Bíblia, aumentando assim, consideravelmente seu vocabulário.
           </p>
-
+          <a href='#curso'><button className='button-ingles'>Veja nossos cursos</button></a>
           </Col>
-
         </Row>
-
-
       </Container>
-      <div className='div-button'>
-      <a href='#curso'><button className='button-ingles'>Veja nossos cursos</button></a>
-      </div>
-
 
     </div>
 
