e2a784aeb18332e8738b644a4ab81a921eb0282e
erica-l-s <erica.l.s1996@gmail.com>
2024-06-29 15:13:56 -0300

    atualizaçao removendo uns imports

--- src/components/Motivacao/Motivacao.js
+++ src/components/Motivacao/Motivacao.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import dinheiro from "./img/ganhar-dinheiro.png";
 import interagir from "./img/interagir.png";
 import viajar from "./img/viagem.png";
