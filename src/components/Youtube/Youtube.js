82f984688b334e2b3e7b29775e38b18feed552af
erica-l-s <erica.l.s1996@gmail.com>
2024-06-30 18:37:56 -0300

    arrumando youtube e cursos

--- src/components/Youtube/Youtube.js
+++ src/components/Youtube/Youtube.js
@@ -18,7 +18,8 @@ export const Youtube = () => {
                             title="YouTube video player"
                             frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowfullscreen='true'>
+                            allowfullscreen='true'
+                            >
                         </iframe>
                         
                     </Col>
